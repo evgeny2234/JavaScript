afa82b6661bee90c90a8b6cea1867ffb940ca163
Евгений Пищиков < >
2016-04-05 17:30:40 +0300

    работа с дочерними элементами, бртьями родителями и т.д. обращение к Ним и их свйствам через javascript

--- moto/js/script.js
+++ moto/js/script.js
@@ -4,6 +4,26 @@ var i = document.getElementsByTagName('img');
 
 alert(i.length);
 
-var j = document.getElementById('rrr');
+var j = document.getElementById('div_for_img');
 
 alert(j.alt);
+
+var x = j.childNodes; //ищем всех дочерних элементов
+
+for (var i=0;i<x.length;i++)
+{
+	x[i].alt = "Fuck";
+	alert(x[i].alt);
+}
+
+var parent = j.parentNode; //ищем родителя
+
+alert(parent.id);
+
+var nextBrother = j.nextSibling;
+
+alert(nextBrother);
+
+var prevBrother = j.previousSibling;
+
+alert(prevBrother);
